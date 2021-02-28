4d89086d7576a853070855418fdec3d23a288c26
priyamishra30 <77337933+priyamishra30@users.noreply.github.com>
2021-02-28 18:19:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var cat,mouse;
+
+
+function preload() {
+    
+
+
+
+
+    //load the images here
+}
+
+function setup(){
+    createCanvas(1000,800);
+    cat = createSprite(200,200,50,80);
+    mouse = createSprite(150,400,50,50);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+background(0,0,0);
+
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    
+  //For moving and changing animation write code here
+
+
+}
